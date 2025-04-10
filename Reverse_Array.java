58a2a457fd168f8c06bf53988b3e843d6a475686
Shaurya Singh <SinghShaurya851@outlook.com>
2025-04-10 06:22:56 +0000

    ReverseArray.java

--- Reverse_Array.java
+++ Reverse_Array.java
@@ -6,11 +6,11 @@ public class Reverse_Array {
         int start = 0;
         int end = arr.length - 1;
         while (start < end) {
-            int temp = arr[start];
+            int temp = arr[start]; 
             arr[start] = arr[end];
             arr[end] = temp;
             start++;
-            end--;
+            end--; 
         }
     }
 
