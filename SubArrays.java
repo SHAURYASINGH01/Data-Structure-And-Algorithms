d4e1230c88d16d4dea64a84aeefc53b0ef3fdd49
Shaurya Singh <SinghShaurya851@outlook.com>
2025-04-10 06:19:04 +0000

    SubArrays.java

--- SubArrays.java
+++ SubArrays.java
@@ -1,1 +1,18 @@
-
+public class SubArrays {
+    public static void PrintSubArray(int nums[]) {
+       for (int i = 0; i<nums.length; i++) {
+            for (int j = i; j<nums.length; j++) {
+                int start = i; int End = j;
+                for (int k = start; k<= End; k++) {
+                    System.out.print(nums[k]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,4,6,8,10};
+        PrintSubArray(nums);
+    }
+}
