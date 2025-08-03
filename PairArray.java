4310326936ef17a171f71085f196ff52a89b4cfb
Shaurya Singh <SinghShaurya851@outlook.com>
2025-08-03 15:38:05 +0530

    PairArray.java

--- PairArray.java
+++ PairArray.java
@@ -1,11 +1,14 @@
 public class Array{
   public static void printpairs(int num[]){
+    int tp=0;
     for(int i=0;i<num.length;i++){
       int curr=num[i];
       for(int j=0;j<num.length;j++){
-        System.out.print("("+ curr + "," + num[j]+")");
+        System.out.print("("+ curr + ","+num[j]+")");
+         tp++;
       }
-      System.out.println();
-    } 
+      System.out.print();
+    }
+    System.out.print("total pairs: "+tp);
   }
 }
