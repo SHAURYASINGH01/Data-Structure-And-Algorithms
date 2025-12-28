5c08b22baeda1f62525dc5439dec786105c05b40
Shaurya Singh😊 <176052807+SHAURYASINGH01@users.noreply.github.com>
2025-12-28 17:32:28 +0530

    PairSum_Array

--- PairSum_Array.java
+++ PairSum_Array.java
@@ -3,12 +3,26 @@ public class Array{
     int tp=0;
     for(int i=0;i<num.length;i++){
       int curr=num[i];
-      for(int j=0;j<num.length;j++){
-        System.out.print("("+ curr + ","+num[j]+")");
+      for(int j=i+1;j<num.length;j++){
+        System.out.println("("+ curr + ","+num[j]+")");
          tp++;
       }
       System.out.print();
     }
-    System.out.print("total pairs: "+tp);
+    System.out.println("total pairs: "+tp);
+  }
+  
+public static void main(String []args){
+  num[] = {2,4,6,8,10};
+  printpairs(num);
   }
 }
+
+
+
+// O/P
+((2,4) (2,6) (2,8) (2,10) //4
+((4,6) (4,8) (4,10))   //3
+((6,8) (6,10))     //2
+(8,10))          //1
+{tp = 10}
